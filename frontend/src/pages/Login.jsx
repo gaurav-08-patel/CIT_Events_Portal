4f8cc2f2a5a1364848448d3483b0093211b360c7
Nikhil-Gupta76 <nikhilgupta8595665@gmail.com>
2026-02-07 22:58:20 +0530

    Added HomePage

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+function Login() {
+  return (
+    <div style={{ padding: "30px" }}>
+
+      <h1>Login Page</h1>
+
+      <form style={{ maxWidth: "300px" }}>
+
+        <input type="email" placeholder="Email" style={inputStyle} />
+        <input type="password" placeholder="Password"style={inputStyle}/>
+        <button style={btnStyle}>Login</button>
+      </form>
+
+    </div>
+  )
+}
+
+const inputStyle = {
+  width: "100%",
+  padding: "8px",
+  marginBottom: "10px"
+}
+
+const btnStyle = {
+  width: "100%",
+  padding: "8px",
+  background: "#38bdf8",
+  border: "none",
+  fontWeight: "bold"
+}
+
+export default Login
