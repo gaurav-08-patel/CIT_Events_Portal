360c36cb3fa9f1310aa793537b26986ef6c8095f
Roshan Patel <roshanpatel12309@gmail.com>
2026-02-09 23:51:20 +0530

    Modified and add isAuth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { isAuthenticated, loginUser, logoutUser, registerUser, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController';
+import { isAuthenticated, loginUser, logoutUser, registerUser, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
 import userAuth from '../middleware/userAuth.js'
 
 const authRouter = express.Router();
@@ -14,6 +14,7 @@ authRouter.post('/logout',logoutUser)
 authRouter.post('/send-verify-otp', userAuth ,sendVerifyOtp)
 authRouter.post('/verify-email', userAuth,verifyEmail)
 authRouter.post('/send-reset-otp', sendResetOtp)
+authRouter.post('/is-authenticated', userAuth, isAuthenticated)
 authRouter.post('/reset-password',resetPassword)
 
 export default authRouter;
