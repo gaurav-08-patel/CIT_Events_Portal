0b7075f2e8874f3ac80794e20e33f61c1285ec7c
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-02-13 21:11:31 +0530

    Completed Login page UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,32 +1,47 @@
-function Login() {
-  return (
-    <div style={{ padding: "30px" }}>
+import { Link } from "react-router-dom";
+import InputBox from "../components/InputBox";
+import { KeyRound } from "lucide-react";
+import Carousel from "../components/Carousel";
 
-      <h1>Login Page</h1>
+const Login = () => {
+    return (
+        <div className="flex min-h-screen p-6 pt-20 bg-linear-to-tr from-slate-300 to-slate-500">
+            <div className="mx-auto flex  h-150 max-lg:h-fit  rounded-lg overflow-hidden shadow">
+                <Carousel />
+                <form className="flex flex-1 flex-col items-center justify-center gap-3 bg-[rgb(242,245,251)] w-md shadow p-6">
+                    <h1 className="text-center text-4xl font-bold max-sm:text-3xl">
+                        Sign In
+                    </h1>
+                    <p className="flex text-lg gap-2">
+                        <p className="text-slate-700 max-sm:text-sm">
+                            Dont have an account?
+                        </p>
+                        <Link
+                            to={"/signup"}
+                            className="text-blue-900 underline max-sm:text-sm"
+                        >
+                            Click here
+                        </Link>
+                    </p>
+                    <InputBox
+                        type="text"
+                        placeholder="Email"
+                        className="mt-5 max-sm:py-2 max-sm:text-sm"
+                    />
 
-      <form style={{ maxWidth: "300px" }}>
-
-        <input type="email" placeholder="Email" style={inputStyle} />
-        <input type="password" placeholder="Password"style={inputStyle}/>
-        <button style={btnStyle}>Login</button>
-      </form>
-
-    </div>
-  )
-}
-
-const inputStyle = {
-  width: "100%",
-  padding: "8px",
-  marginBottom: "10px"
-}
-
-const btnStyle = {
-  width: "100%",
-  padding: "8px",
-  background: "#38bdf8",
-  border: "none",
-  fontWeight: "bold"
-}
+                    <InputBox
+                        icon={KeyRound}
+                        type="password"
+                        placeholder="Password"
+                        className="max-sm:py-2 max-sm:text-sm"
+                    />
+                    <button className="max-sm:text-sm text-white tracking-widest font-semibold cursor-pointer bg-[rgb(58,60,165)] px-7 py-3 rounded-full">
+                        SIGN IN
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
 
 export default Login;
