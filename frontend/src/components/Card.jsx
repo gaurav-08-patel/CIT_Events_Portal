5c96d13489fe7de30e6b5370e390ac824894b0fe
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-02-24 20:35:41 +0530

    made homepage responsive and made card to accept dynamic data

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,67 +1,70 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Calendar, MapPin, Users, IndianRupee } from "lucide-react";
+import { BsPersonStanding } from "react-icons/bs";
 
-function Card({ card }) {
-  const navigate = useNavigate();
+function Card({ card: event }) {
+    function formatTime24to12(time24) {
+        const [hourStr, minuteStr] = time24.split(":");
+        let hour = Number(hourStr);
+        const minutes = minuteStr;
 
-  return (
-    <div
-      onClick={() => navigate(`/card/${card.id}`)}
-      className="
-        bg-[#2f4154]
-        text-white
-        w-full
-        sm:w-70
-        md:w-87.5
-        rounded-xl
-        shadow-lg
-        p-5
-        cursor-pointer
-        transition
-        duration-300
-        hover:scale-105
-        hover:shadow-2xl
-      "
-    >
-      {/* Title */}
-      <h3 className="text-xl font-serif text-yellow-300 text-center mb-3">
-        {card.title}
-      </h3>
+        const ampm = hour >= 12 ? "PM" : "AM";
+        hour = hour % 12 || 12;
 
-      {/* Image */}
-      <img
-        src={card.image}
-        alt={card.title}
-        className="w-full h-40 object-cover rounded-md mb-3"
-      />
+        return `${hour}:${minutes} ${ampm}`;
+    }
 
-      {/* Content */}
-      <p className="text-sm text-center text-gray-200 mb-4">{card.content}</p>
+    return (
+        <div className="bg-[#2f4154] text-white shadow-md rounded-lg p-6 flex flex-col justify-between">
+            {/* Event Title */}
+            <h2 className=" text-2xl font-semibold line-clamp-3 text-yellow-300 text-center mb-3 min-h-23.75">
+                {event.name}
+            </h2>
 
-      {/* Info Section */}
-      <div className="space-y-2 text-sm text-gray-300">
-        <div className="flex items-center gap-2">
-          <Calendar size={16} />
-          <span>2026-02-10</span>
+            {/* Event Details */}
+            <div className="space-y-2 text-sm text-gray-300 mt-auto">
+                <div className="flex items-center gap-2">
+                    <Calendar size={16} />
+                    <span>
+                        {formatTime24to12(event.time)} on {event.date}
+                    </span>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <MapPin size={16} />
+                    <span className="line-clamp-1">{event.venue}</span>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <IndianRupee size={16} />
+                    <span>{event.entryType}</span>
+                    {event.entryType === "Paid" &&
+                        ` (${event?.entryAmount} per person)`}
+                </div>
+
+                <div className="flex items-center gap-2">
+                    {event.participantsAllowedInTeam === 1 ? (
+                        <BsPersonStanding size={16} />
+                    ) : (
+                        <Users size={16} />
+                    )}
+
+                    <span>
+                        {event.participantsAllowedInTeam > 1
+                            ? `1 - ${event.participantsAllowedInTeam}`
+                            : `1`}
+                    </span>
+                </div>
+            </div>
+
+            {/* Action Button */}
+            <Link to={`event/${event.id}`}>
+                <button className="cursor-pointer w-full mt-4 px-4 py-2 bg-[rgb(0,188,255)] text-white rounded hover:bg-blue-700 transition transition-duration-300">
+                    Participate
+                </button>
+            </Link>
         </div>
-
-        <div className="flex items-center gap-2">
-          <MapPin size={16} />
-          <span>Campus / Online</span>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <IndianRupee size={16} />
-          <span>150</span>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Users size={16} />
-          <span>1-10</span>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Card;
