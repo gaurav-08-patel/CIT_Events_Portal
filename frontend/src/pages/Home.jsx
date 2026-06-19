b0428c58d189bbd8d778a5485cfe6867d3e8ac76
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-19 20:22:55 +0545

    added to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "../components/HeroSection";
+import { StatsSection } from "../components/StatsSection";
 
 const Home = () => {
     return (
         <div>
             <HeroSection />
+            <StatsSection />
         </div>
     );
 };
