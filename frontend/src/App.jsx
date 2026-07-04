c404a6a870498860ccce65709ef8ee34f44a75d6
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-07-04 16:45:49 +0530

    feat: add Login route to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { EventsPage } from "./pages/EventsPage";
+import Login from "./pages/Login";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/events" element={<EventsPage />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     );
 }
