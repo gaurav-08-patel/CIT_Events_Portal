658c4d6380dc62c52bb869f7698c826a13c9c0d4
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-07-05 10:18:54 +0530

    added Forgot password to app route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { EventsPage } from "./pages/EventsPage";
 import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 import Register from "./pages/Register";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/events" element={<EventsPage />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/register" element={<Register />} />
         </Routes>
     );
