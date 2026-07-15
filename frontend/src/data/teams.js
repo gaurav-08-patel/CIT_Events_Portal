28e9adc97b01f3a4565e9f324097229ec966e806
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-07-15 21:35:43 +0530

    added new team members

--- frontend/src/data/teams.js
+++ frontend/src/data/teams.js
@@ -60,4 +60,62 @@ export const initialTeams = [
         createdAt: new Date("2025-01-15").toISOString(),
         status: "active",
     },
+    {
+        id: "team-005",
+        name: "Halo Coders",
+        leaderId: 23,
+        members: [
+            {
+                id: 23,
+                email: "youremail@example.edu.in",
+                name: "John Kumar Doe",
+            },
+            {
+                id: "tm-001",
+                email: "teammate1@example.edu.in",
+                name: "Teammate One",
+            },
+            {
+                id: "tm-002",
+                email: "teammate2@example.edu.in",
+                name: "Teammate Two",
+            },
+            {
+                id: "tm-003",
+                email: "teammate3@example.edu.in",
+                name: "Teammate Three",
+            },
+        ],
+        createdAt: new Date("2025-01-15").toISOString(),
+        status: "active",
+    },
+    {
+        id: "team-009",
+        name: "Vibe Coders",
+        leaderId: 23,
+        members: [
+            {
+                id: 23,
+                email: "youremail@example.edu.in",
+                name: "John Kumar Doe",
+            },
+            {
+                id: "tm-001",
+                email: "teammate1@example.edu.in",
+                name: "Teammate One",
+            },
+            {
+                id: "tm-002",
+                email: "teammate2@example.edu.in",
+                name: "Teammate Two",
+            },
+            {
+                id: "tm-003",
+                email: "teammate3@example.edu.in",
+                name: "Teammate Three",
+            },
+        ],
+        createdAt: new Date("2025-01-15").toISOString(),
+        status: "active",
+    },
 ];
